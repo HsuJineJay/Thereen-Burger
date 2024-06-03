36f9871de1f3e8f8ed9c71ffca14ffcb2cf5e613
CGJay <gogickvictorique@gmail.com>
2024-06-03 11:41:18 +0800

    mainpage banner hover animation  home.js

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+/*
 window.onload = function () {
     $('a').on('click', function () {
         var theID = $(this).attr('id');
@@ -13,3 +14,29 @@ window.onload = function () {
       });
       
 }
+*/
+
+
+// 首頁滑鼠移動鍵變大banner
+document.addEventListener('DOMContentLoaded', () => {
+  const interBubble = document.querySelector('.interactive');
+  let curX = 0;
+  let curY = 0;
+  let tgX = 0;
+  let tgY = 0;
+
+  const move = () => {
+    curX += (tgX - curX) / 20;
+    curY += (tgY - curY) / 20;
+    interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+    requestAnimationFrame(move);
+  };
+
+  window.addEventListener('mousemove', (event) => {
+    tgX = event.clientX;
+    tgY = event.clientY;
+  });
+
+  move();
+});
+
